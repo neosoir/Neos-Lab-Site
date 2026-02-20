65a4c67adf9f46bc6bcd1711b54110812c1e891c
Neo <neo@neoslab.dev>
2026-02-20 13:05:34 -0600

    [tweak] main css into default page

--- frontend/src/componets/Footer.tsx
+++ frontend/src/componets/Footer.tsx
@@ -5,19 +5,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className='footer'>
-      <div className='footer__main'>
-        <div className='footer__legal'>
-          <h4>Datos fiscales</h4>
-          <p>Leonardo Rios Pineda</p>
-          <p>RFC: RIPL990424HT7</p>
-          <p>neoslab.marketing@gmail.com</p>
-          <div className="footer__links">
-            <Link to="/privacidad">Política de Privacidad</Link>
-            <Link to="/terminos">Términos del Servicio</Link>
-          </div>
-        </div>
-      </div>
-      
+
       <div className='footer__social'>
         <a className="link" title="Platzi" href="https://platzi.com/p/leonard-rios/" target="_blank" rel="noopener noreferrer"><SiPlatzi /></a>
         <a className="link" title="Linkeding" href="https://mx.linkedin.com/in/leonardo-rios-pineda-979417216" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
@@ -26,9 +14,20 @@ function Footer() {
         <a className="link" title="Email" href="mailto:[Email]" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
         <a className="link" title="Git Hub" href="https://github.com/neosoir" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
       </div>
-
       <div className='footer__copyright'>
         <p>© 2025 Neos Lab. Todos los derechos reservados.</p>
+      </div>
+      
+      <div className='footer__legal'>
+        <h4>Datos fiscales</h4>
+        <span>Leonardo Rios Pineda</span>
+        <span>RFC: RIPL990424HT7</span>
+        <span>neoslab.marketing@gmail.com</span>
+      </div>
+
+      <div className="footer__links">
+        <Link to="/privacidad">Política de Privacidad</Link>
+        <Link to="/terminos">Términos del Servicio</Link>
       </div>
     </footer>
   );
